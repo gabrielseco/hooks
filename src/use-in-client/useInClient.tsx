bd5946003c5d8d29f9f3366a96bb838e7b05ba9d
Gabriel García <ggarciaseco@gmail.com>
2020-12-13 17:53:18 +0100

    Improve useClient hooks

--- src/use-in-client/useInClient.tsx
+++ src/use-in-client/useInClient.tsx
@@ -3,19 +3,22 @@ import React from 'react';
 export function useInClient<T>(callback: () => T): T | null {
   const [valueCallback, setValueCallback] = React.useState<T | null>(null);
   const [isClient, setClient] = React.useState(false);
+  const refCallback = React.useRef<unknown | null>(null);
 
-  const memoizedCallback = React.useCallback(() => callback(), [callback]);
+  React.useLayoutEffect(() => {
+    refCallback.current = callback;
+  });
 
   React.useEffect(() => {
     setClient(true);
   }, []);
 
   React.useEffect(() => {
-    if (isClient) {
-      const calculatedValue = memoizedCallback();
+    if (isClient && refCallback.current) {
+      const calculatedValue = (refCallback as any).current();
       setValueCallback(calculatedValue);
     }
-  }, [isClient, memoizedCallback]);
+  }, [isClient]);
 
   return valueCallback;
 }
